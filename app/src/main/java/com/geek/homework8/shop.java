282efc836d30f08243bbe5197bd6b575cd4040e4
Lira09 <nazirbekova.lira@gmail.com>
2021-04-27 18:35:38 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/homework8/shop.java
@@ -0,0 +1,14 @@
+package com.geek.homework8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
